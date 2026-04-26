900e8308a2b5a8678ad15026b1c7b291a7c40256
tashfinsami <tashfinsami@gmail.com>
2026-04-26 14:56:15 +0600

    hello world using threading

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class HelloWorld extends Thread {
+    private String tname;
+    public HelloWorld(String tname) {
+        this.tname = tname;
+    }
+    @Override
+    public void run() {
+        System.out.println("Hello World from " + tname);
+    }
+
+    public static void main(String args[]) {
+        HelloWorld thread1 = new HelloWorld("thread 0");
+        HelloWorld thread2 = new HelloWorld("thread 1");
+        thread1.start();
+        thread2.start();
     }
 }
